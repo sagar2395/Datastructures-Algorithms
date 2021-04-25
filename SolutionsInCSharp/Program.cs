fc7eb7f9e364a2bfd627423cd2a3e00e6fb4982e
Sagar Chhabra <sagar.chhabra02@gmail.com>
2021-04-25 20:02:19 +0530

    removed bin and obj by adding them to .gitignore

--- SolutionsInCSharp/Program.cs
+++ SolutionsInCSharp/Program.cs
@@ -7,6 +7,7 @@ namespace SolutionsInCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Sagar chhabra");
         }
     }
 }

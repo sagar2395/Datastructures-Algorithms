2590862959c455dd03bad038f25f8b19268464d4
Sagar Chhabra <sagar.chhabra02@gmail.com>
2021-04-26 18:35:33 +0530

    Deleted 1.cs

--- SolutionsInCSharp/Array/1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace SolutionsInCSharp.Array {
-    public class MyArray {
-        public void printHelloWorld(){
-            Console.WriteLine("Inside print hello world in array");
-        }
-    }
-}

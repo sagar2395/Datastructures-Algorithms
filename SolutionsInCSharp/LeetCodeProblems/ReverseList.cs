ca927cbdfce91a1ca2bcd64292d05e1abe03d1c7
Sagar Chhabra <sagar.chhabra02@gmail.com>
2021-05-15 00:55:02 +0530

    Reverse List problem

---
+++ SolutionsInCSharp/LeetCodeProblems/ReverseList.cs
@@ -0,0 +1,22 @@
+namespace SolutionsInCSharp
+{
+    public class Solution30
+    {
+        public ListNode reverseList(ListNode head)
+        {
+            ListNode current = head;
+            ListNode previous = null;
+            ListNode temp;
+
+            while (current != null)
+            {
+                temp = current.next;
+                current.next = previous;
+                previous = current;
+                current = temp;
+            }
+
+            return previous;
+        }
+    }
+}

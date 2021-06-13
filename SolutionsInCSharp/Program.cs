30c329a446dc8a1e8a414ecf77f4baa11023bd72
Sagar Chhabra <sagar.chhabra02@gmail.com>
2021-06-13 13:18:53 +0530

    LINQ practise

--- SolutionsInCSharp/Program.cs
+++ SolutionsInCSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SolutionsInCSharp;
+using System.Linq;
 
 namespace SolutionsInCSharp
 {
@@ -7,8 +8,16 @@ namespace SolutionsInCSharp
     {
         static void Main(string[] args)
         {
+            int[] a = new int[]{5,10,12,2,6,45,78,85,1,0,5,52,78};
 
+            double b = a.Where(x => x % 2 == 0).Select(x => x * 100).OrderBy(x => x).Skip(2).Take(3).Min();
 
+            //foreach(var i in b){
+                Console.WriteLine(b);
+            //}
+
+            Console.WriteLine("End of Program");
+        
 
         }
     }

433d380541232b931ec7a227873e965570127a6f
Sagar Chhabra <sagar.chhabra02@gmail.com>
2021-06-21 21:03:59 +0530

    String Backspace compare solution completed

--- SolutionsInCSharp/LeetCode/BackSpaceStringCompare.cs
+++ SolutionsInCSharp/LeetCode/BackSpaceStringCompare.cs
@@ -1,7 +1,30 @@
+using System;
+using System.Text;
+
 namespace SolutionsInCSharp{
     public class Solution46{
         public bool BackspaceCompare(string s, string t){
-            return false;
+
+            string a = GetFinalString(s);
+            string b = GetFinalString(t); 
+
+            return a.Equals(b);
+        }
+
+        public string GetFinalString(string value){
+            StringBuilder s = new StringBuilder();
+
+            foreach(var a in value){
+                if(a == '#' && s.Length != 0){
+                    s.Remove(s.Length - 1, 1);
+                }
+                else{
+                    s.Append(a);
+                }
+            }
+
+            return s.ToString();
+
         }
     }
 }
--- SolutionsInCSharp/Program.cs
+++ SolutionsInCSharp/Program.cs
@@ -8,16 +8,10 @@ namespace SolutionsInCSharp
     {
         static void Main(string[] args)
         {
-            Solution25 rs = new Solution25();
+            Solution46 rs = new Solution46();
 
-            int[] a = new int[]{5 , 13, 18, 24, 36};
-            int[] b = new int[]{8 , 12, 14, 28, 39, 53};
-        
-            int[] c = rs.MergeSortedArrays(a , b);
+            Console.WriteLine(rs.BackspaceCompare("Hello#####" , ""));
             
-            foreach(var val in c){
-                Console.Write(val + " ");
-            }
         }
     }
 }

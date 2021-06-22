0a25e6a1a627cf83c1c15d83174545d0b307d251
Sagar Chhabra <sagar.chhabra02@gmail.com>
2021-06-22 14:48:43 +0530

    Backspace string compare alternative solution

--- SolutionsInCSharp/LeetCode/BackSpaceStringCompare.cs
+++ SolutionsInCSharp/LeetCode/BackSpaceStringCompare.cs
@@ -1,32 +1,85 @@
 using System;
 using System.Text;
 
-namespace SolutionsInCSharp{
-    public class Solution46{
-        public bool BackspaceCompare(string s, string t){
+namespace SolutionsInCSharp
+{
+    public class Solution46
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
 
             string a = GetFinalString(s);
-            string b = GetFinalString(t); 
+            string b = GetFinalString(t);
 
             return a.Equals(b);
         }
 
-        public string GetFinalString(string value){
+        public string GetFinalString(string value)
+        {
             StringBuilder s = new StringBuilder();
 
-            foreach(var a in value){
-                if(a == '#'){
-                    if(s.Length != 0){
+            foreach (var a in value)
+            {
+                if (a == '#')
+                {
+                    if (s.Length != 0)
+                    {
                         s.Remove(s.Length - 1, 1);
                     }
                 }
-                else{
+                else
+                {
                     s.Append(a);
                 }
             }
 
             return s.ToString();
 
+        }
+
+        public bool BackspaceCompare2(string s, string t)
+        {
+            int a = s.Length - 1;
+            int b = t.Length - 1;
+            
+            while (a >= 0 || b >= 0)
+            {
+                if(s[a] == '#' || t[b] == '#'){
+                    if(s[a] == '#'){
+                        int backCount = 2;
+                        while(backCount > 0 ){
+                            a--;
+                            backCount--;
+                            if(s[a] == '#'){
+                                backCount += 2;
+                            }
+                        }
+                    }
+
+                    if(s[b] == '#'){
+                        int backCount = 2;
+                        while(backCount > 0 ){
+                            b--;
+                            backCount--;
+                            if(s[b] == '#'){
+                                backCount += 2;
+                            }
+                        }
+                    }
+                }
+                else{
+                    if(s[a] == t[b])
+                        return false;
+                    else{
+                        a--;
+                        b--;
+                    }
+                }
+
+            }
+
+
+            return true;
         }
     }
 }

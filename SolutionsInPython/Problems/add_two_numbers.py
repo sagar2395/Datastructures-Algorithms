de4db48af42fe5537a3feb7eed77b64d19aba0aa
Sagar Chhabra <sagar.chhabra02@gmail.com>
2024-10-14 23:49:05 +0530

    Solving add two numbers problem

--- SolutionsInPython/Problems/add_two_numbers.py
+++ SolutionsInPython/Problems/add_two_numbers.py
@@ -1,15 +1,37 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 def addNTwoNumbers(l1, l2):
     temp, l = l1,l2
-    resultNode = l1.val + l2.val
     c = 0
-    
-    
+    resultNode = ListNode((temp.val + l.val + c) % 10)
+    c = (temp.val + l.val + c) / 10  
+    resultNodetemp = resultNode
+    l = l.next
+    temp = temp.next     
     while temp != None and l != None:
-        resultNode.next = temp.val + l.val
+        val = (temp.val + l.val + c) % 10
+        resultNodetemp.next = ListNode(val) 
+        resultNodetemp = resultNodetemp.next
+        c = (temp.val + l.val + c) / 10
         temp = temp.next
         l = l.next
-        resultNode = resultNode.next
+    while temp != None:
+        resultNodetemp.next = ListNode((temp.val + c) % 10)
+        c = (temp.val + c) / 10
+        resultNodetemp = resultNodetemp.next
+        temp = temp.next
+    while l != None:
+        resultNodetemp = ListNode((l.val + c) % 10)
+        c = (l.val + c) / 10
+        resultNodetemp = resultNodetemp.next
+        l = l.next
+    
+    if c != 0:
+        resultNodetemp.next = ListNode(c)
+        
+    return resultNode
         
         
-        
-# Not yet completed. To Do

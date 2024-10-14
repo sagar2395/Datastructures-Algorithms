94bdf8f5518010964faac7188fc4800016259f4a
Sagar Chhabra <sagar.chhabra02@gmail.com>
2024-10-15 00:30:21 +0530

    Added corrections for add two numbers using linked list

--- SolutionsInPython/Problems/add_two_numbers.py
+++ SolutionsInPython/Problems/add_two_numbers.py
@@ -8,30 +8,32 @@ def addNTwoNumbers(l1, l2):
     c = 0
     resultNode = ListNode((temp.val + l.val + c) % 10)
     c = (temp.val + l.val + c) / 10  
-    resultNodetemp = resultNode
-    l = l.next
-    temp = temp.next     
-    while temp != None and l != None:
-        val = (temp.val + l.val + c) % 10
+    resultNodetemp = resultNode    
+    while temp.next != None and l.next != None:
+        val = (temp.next.val + l.next.val + c) % 10
         resultNodetemp.next = ListNode(val) 
         resultNodetemp = resultNodetemp.next
-        c = (temp.val + l.val + c) / 10
+        c = (temp.next.val + l.next.val + c) / 10
         temp = temp.next
         l = l.next
-    while temp != None:
-        resultNodetemp.next = ListNode((temp.val + c) % 10)
-        c = (temp.val + c) / 10
+    while temp.next != None:
+        resultNodetemp.next = ListNode((temp.next.val + c) % 10)
+        c = (temp.next.val + c) / 10
         resultNodetemp = resultNodetemp.next
         temp = temp.next
-    while l != None:
-        resultNodetemp = ListNode((l.val + c) % 10)
-        c = (l.val + c) / 10
+    while l.next != None:
+        resultNodetemp.next = ListNode((l.next.val + c) % 10)
+        c = (l.next.val + c) / 10
         resultNodetemp = resultNodetemp.next
         l = l.next
-    
-    if c != 0:
-        resultNodetemp.next = ListNode(c)
+    while c != 0:
+        resultNodetemp.next = ListNode(c % 10)
+        c = c / 10
+        resultNodetemp = resultNodetemp.next
         
     return resultNode
+
         
+
+
         

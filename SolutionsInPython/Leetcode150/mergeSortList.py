87c2a5c0f12192de0943c283b0eb41a5ebcebbcf
Sagar Chhabra <sagar.chhabra02@gmail.com>
2024-10-28 01:10:07 +0530

    Adding divide and conquer problems

---
+++ SolutionsInPython/Leetcode150/mergeSortList.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        left = head
+
+        def findMid(head):
+            slow = head
+            fast = head.next
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+
+        mid = findMid(head)
+
+        right = mid.next
+        mid.next = None
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        def merge(left, right):
+            dummy = ListNode(0)
+            temp = dummy
+            while left and right:
+                if left.val < right.val:
+                    temp.next = left
+                    left = left.next
+                else:
+                    temp.next = right
+                    right = right.next
+            
+            if left:
+                temp.next = left
+            if right:
+                temp.next = right
+
+            return dummy.next
+        return merge(left, right)

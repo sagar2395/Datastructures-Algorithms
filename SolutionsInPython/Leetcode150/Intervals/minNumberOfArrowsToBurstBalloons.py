c961982a6c18dd6bc1f1e61ea900b7c023077723
Sagar Chhabra <sagar.chhabra02@gmail.com>
2024-11-06 21:57:27 +0530

    Adding linked list problem

---
+++ SolutionsInPython/Leetcode150/Intervals/minNumberOfArrowsToBurstBalloons.py
@@ -0,0 +1,39 @@
+# 452. Minimum Number of Arrows to Burst Balloons
+# https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/?envType=study-plan-v2&envId=top-interview-150
+
+# Example 1:
+# Input: points = [[10,16],[2,8],[1,6],[7,12]]
+# Output: 2
+# Explanation: The balloons can be burst by 2 arrows:
+# - Shoot an arrow at x = 6, bursting the balloons [2,8] and [1,6].
+# - Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12].
+
+# Example 2:
+# Input: points = [[1,2],[3,4],[5,6],[7,8]]
+# Output: 4
+# Explanation: One arrow needs to be shot for each balloon for a total of 4 arrows.
+
+# Example 3:
+# Input: points = [[1,2],[2,3],[3,4],[4,5]]
+# Output: 2
+# Explanation: The balloons can be burst by 2 arrows:
+# - Shoot an arrow at x = 2, bursting the balloons [1,2] and [2,3].
+# - Shoot an arrow at x = 4, bursting the balloons [3,4] and [4,5].
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        
+        points.sort()
+
+        res = len(points)
+        prev = points[0]
+        for i in range(1, len(points)):
+            curr = points[i]
+
+            if curr[0] <= prev[1]:
+                res -= 1
+                prev = [curr[0], min(prev[1], curr[1])]
+            else:
+                prev = curr
+        
+        return res
---
+++ SolutionsInPython/Leetcode150/LinkedList/ReverseKGroup.py
@@ -0,0 +1,49 @@
+# 25. Reverse Nodes in k-Group
+# https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+
+# Example 1:
+# Input: head = [1,2,3,4,5], k = 2
+# Output: [2,1,4,3,5]
+
+# Example 2:
+# Input: head = [1,2,3,4,5], k = 3
+# Output: [3,2,1,4,5]s
+
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        groupPrev= dummy
+
+        while True:
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+
+            prev, curr = kth.next, groupPrev.next
+
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+            
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
+
+

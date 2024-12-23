b75245fc37c71770720a974ec7cbdd74960ed761
Sagar Chhabra <sagar.chhabra02@gmail.com>
2024-12-23 11:11:33 +0530

    Adding sort list problem

--- SolutionsInPython/Leetcode150/mergeSortList.py
+++ SolutionsInPython/Leetcode150/mergeSortList.py
@@ -1,3 +1,17 @@
+# 148. Sort List
+# https://leetcode.com/problems/sort-list/description/
+
+# Example 1:
+# Input: head = [4,2,1,3]
+# Output: [1,2,3,4]
+
+# Example 2:
+# Input: head = [-1,5,3,4,0]
+# Output: [-1,0,3,4,5]
+
+# Example 3:
+# Input: head = []
+# Output: []
 
 from typing import Optional, List
 # Definition for singly-linked list.
@@ -48,3 +62,37 @@ class Solution:
 
             return dummy.next
         return merge(left, right)
+    
+    # Approach 2 for sorting list
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        mid = slow.next
+        slow.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        return self.merge(left, right)
+
+    def merge(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+
+        tail.next = l1 if l1 else l2
+        return dummy.next
